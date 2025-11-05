827a22da2a3984d1fdce205abbf98957340f37dc
Himanshu Kumar <anku@Himanshus-MacBook-Air.local>
2025-11-06 00:01:31 +0530

    dist env

--- dist/validators/conversation.validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createConversationSchema = void 0;
-const zod_1 = require("zod");
-exports.createConversationSchema = zod_1.z.object({
-    participantIds: zod_1.z.array(zod_1.z.string().min(1)).min(1)
-});
--- dist/validators/message.validator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendMessageSchema = void 0;
-const zod_1 = require("zod");
-exports.sendMessageSchema = zod_1.z.object({
-    conversationId: zod_1.z.string().min(1),
-    content: zod_1.z.string().min(1),
-    attachments: zod_1.z.array(zod_1.z.string().url()).optional()
-});
